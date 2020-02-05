b48f12315a17e37468ffef574dea7ffea516580b
mhyun <michaelhhyun@gmail.com>
2020-02-05 14:45:51 -0500

    Exercise Completed

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -23,6 +23,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameBinding
 
@@ -101,12 +102,16 @@ class GameFragment : Fragment() {
                         // TODO (05) Find the navController from the view and navigate to the gameWonFragment
                         // call view.findNavController
                         // navigate to R.id.action_gameFragment_to_gameWonFragment
+                        view.findNavController()
+                                .navigate(R.id.action_gameFragment_to_gameWonFragment)
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
                     // TODO (06) Find the navController from the view and navigate to the gameOverFragment
                     // call view.findNavController
                     // navigate to R.id.action_gameFragment_to_gameOverFragment
+                    view.findNavController()
+                            .navigate(R.id.action_gameFragment_to_gameOverFragment2)
                 }
             }
         }
